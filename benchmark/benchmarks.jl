1b75bcf584834b410f5432d3ed86ce715947ff3c
Huckleberry Febbo <febbo@umich.edu>
2017-07-06 00:47:34 -0400

    fixed issue with benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,12 +1,12 @@
 using PkgBenchmark, MAVs, NLOptControl
 
-@benchgroup "psMethods"  begin
+@benchgroup "psMethods"  ["integrationScheme","Nck"] begin
     for scheme in (:lgrExplicit,:lgrImplicit)
         for Nck in ([10,8,6],[12,10,8,6])
           c=defineCase(;(:mode=>:autoBench));
           setMisc!(c;integrationScheme=scheme,Nck=Nck,max_cpu_time=20.0)
           n=initializeAutonomousControl(c);
-          @bench "optimize" optimize!($n)
+          @bench string(scheme), Nck  optimize!($n)
         end
     end
 end
