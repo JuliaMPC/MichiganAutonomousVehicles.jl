d46f2dc809217a09ac0dd79d36ee0cadf721346d
Huckleberry Febbo <febbo@umich.edu>
2017-07-15 16:24:26 -0400

    build test

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,7 +5,8 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
-subdirs(mavs_description)
 subdirs(mavs_gazebo)
 subdirs(mavs_simulator)
+subdirs(mavs_description)
+subdirs(mavs_msgs)
 subdirs(mavs_plugins)
--- catkin_ws/build/mavs_description/cmake_install.cmake
+++ catkin_ws/build/mavs_description/cmake_install.cmake
@@ -33,6 +33,10 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
 endif()
 
 if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/mavs_description/srv" TYPE FILE FILES "/home/febbo/.julia/v0.6/MAVs/catkin_ws/src/mavs_description/srv/AddTwoInts.srv")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/febbo/.julia/v0.6/MAVs/catkin_ws/build/mavs_description/catkin_generated/installspace/mavs_description.pc")
 endif()
 
