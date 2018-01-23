e0184e40fd378ef9b7ef2d1df8233b26876bdd5d
Huckleberry Febbo <febbo@umich.edu>
2018-01-23 18:12:31 -0500

    added changes..

--- examples/sharedControl/tune_gains.jl
+++ examples/sharedControl/tune_gains.jl
@@ -3,7 +3,7 @@ try
     close(yingshi)
 end
 yingshi = UDPSocket();
-bind(yingshi,ip"141.212.140.176",13000); # change this ip to the server (or julia code)
+bind(yingshi,ip"73.108.52.5",13000); # change this ip to the server (or julia code)
 
 gains=[30.0,0.15,3.0];
 function tune(gains)
@@ -13,7 +13,7 @@ function tune(gains)
   end
   MsgOutString = string(MsgOutString," \n");
   send(yingshi,ip"141.212.141.245",36896,MsgOutString);
-   # change this to the ip where you are running Simulink!
+  # change this to the ip where you are running Simulink!
   print(MsgOutString)
 end
 
