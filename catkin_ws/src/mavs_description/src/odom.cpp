989f7ac0f4223d64d4e11fc2db491e5465acddb7
Huckleberry Febbo <febbo@umich.edu>
2018-01-23 18:50:02 -0500

    few changes..

--- catkin_ws/src/mavs_description/src/odom.cpp
+++ catkin_ws/src/mavs_description/src/odom.cpp
@@ -13,9 +13,9 @@ int main(int argc, char** argv){
   double y = 0.0;
   double th = 0.0;
 
-  double vx = 0.1;
-  double vy = -0.1;
-  double vth = 0.1;
+  double vx = 0.0;
+  double vy = 0.0;
+  double vth = 0.0;
 
   ros::Time current_time, last_time;
   current_time = ros::Time::now();
